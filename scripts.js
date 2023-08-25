aab595ea96842c037ea0cf02ab1f8a8f49fa8cd7
Gio Ciudadano <gaciudadano@up.edu.ph>
2023-08-25 10:11:07 +0800

    Update to VueJS Framework

--- scripts.js
+++ scripts.js
@@ -1,10 +1,17 @@
-function scrollToSection(sectionName) {
-  document
-    .getElementById(String(sectionName))
-    .scrollIntoView({ behavior: 'smooth', block: 'start' });
-}
+/*
+ * Name: scripts.js
+ * Author: Gio Carlo Ciudadano
+ * Description:
+ * A collection of Javascript functions for the portfolio website
+ * giociudadano.github.io.
+ */
 
-function updateScrollbar(e) {
+/* updateNavigationBar(event)
+ * - Listens for the position of the scrollbar in the viewport and updates the
+ *   visibility of the navigation bar.
+ */
+
+function updateNavigationBar(e) {
   const navBarPost = document.querySelector('.nav-bar-sticky');
   let scrollYPos = this.scrollY;
   if (scrollYPos > 200) {
@@ -14,4 +21,15 @@ function updateScrollbar(e) {
   }
 }
 
-window.addEventListener('scroll', updateScrollbar);
+window.addEventListener('scroll', updateNavigationBar);
+
+/* scrollToSection(sectionName)
+ * - Listens for the position of the scrollbar in the viewport and updates the
+ *   visibility of the navigation bar if the user scrolls above or below 200px.
+ */
+
+function scrollToSection(sectionName) {
+  document
+    .getElementById(String(sectionName))
+    .scrollIntoView({ behavior: 'smooth', block: 'start' });
+}
